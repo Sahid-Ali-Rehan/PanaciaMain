aad866613f0aaf3f00b61314df7cf4c230bd970f
sahid ali <sahidalirehan@gmail.com>
2024-11-20 13:34:29 +0600

    Initial commit after reinitializing Git

--- src/Pages/Categories/Categories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Categories = () => {
-  return (
-    <div className='h-screen w-full bg-black-200'>Categories</div>
-  )
-}
-
-export default Categories
--- src/Pages/index.js
+++
@@ -1,8 +0,0 @@
-const Home = () => (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
-      <h1 className="text-white text-4xl font-bold">Welcome to MyBrand!</h1>
-    </div>
-  );
-  
-  export default Home;
-  
